0e9e658e624ab3272ca8d49076fc231076754e91
fazzy12 <kaluifeanyi00@gmail.com>
2023-04-05 20:12:34 +0100

    Import wait_random from the previous python file

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+contains an asynchronous coroutine that takes in two 
+integer arguments and returns a list of random
+delays printed a number of times. The list should be in
+ascending order.
+"""
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    asynchronous coroutine that takes in two
+    integer arguments (n and max_delay) and returns a list
+    of all the delays (float values). The number of delays
+    is the same as n. The list should be in ascending order
+    without using sort().
+    Returns:
+        A list of all the delays (float values).
+    """
+    wait_list = []
+
+    for i in range(n):
+        wait_list.append(await wait_random(max_delay))
+
+    return sorted(wait_list)
