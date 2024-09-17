b192f69c2463becfde2e30c422e75226af120b8d
fazzy12 <kaluifeanyi00@gmail.com>
2024-09-17 14:43:35 +0100

    returns the result as a float.

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+from typing import List, Union
+
+"""function will have type annotations using
+the typing module to specify
+the expected input type (List[Union[int, float]])
+and return type (float)."""
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """return type is always a float."""
+    return float(sum(mxd_lst))
