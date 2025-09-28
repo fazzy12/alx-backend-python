efd114616ff5dc4fc5593381d6609544e0a3c175
ifeanyi kalu <kaluifeanyi00@gmail.com>
2025-09-28 18:07:07 +0100

    Implement Authenticatio

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,25 +1,31 @@
 from rest_framework import permissions
+from django.shortcuts import get_object_or_404
 
-class IsParticipantOfConversation(permissions.BasePermission): # <-- RENAMED CLASS
+class IsParticipantOfConversation(permissions.BasePermission): 
     """
     Custom permission to only allow participants of a conversation 
     to send, view, update, and delete messages and conversations.
-
-    Note: The IsAuthenticated check is handled by the global setting 
-    or by adding IsAuthenticated to the ViewSet's permission_classes.
     """
 
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            if hasattr(obj, 'conversation'):
-                return request.user in obj.conversation.participants.all()
-            if hasattr(obj, 'participants'):
-                return request.user in obj.participants.all()
-        
-        if hasattr(obj, 'conversation'):
-             return request.user in obj.conversation.participants.all()
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
 
-        if hasattr(obj, 'participants'):
-             return request.user in obj.participants.all()
-             
-        return False
+    def has_object_permission(self, request, view, obj):
+        
+        is_participant = False
+
+        if hasattr(obj, 'conversation'):
+            if request.user in obj.conversation.participants.all():
+                is_participant = True
+        
+        elif hasattr(obj, 'participants'):
+            if request.user in obj.participants.all():
+                is_participant = True
+
+        if request.method in permissions.SAFE_METHODS:
+            return is_participant
+        
+        if request.method in ['PUT', 'PATCH', 'DELETE']:
+            return is_participant
+        
+        return is_participant
